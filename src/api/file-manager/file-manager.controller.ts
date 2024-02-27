f009bc03fc635d15543beb6ee48323fe4d87fff3
Mohit Prakash <mohit.prakash@antiersolutions.com>
2024-02-27 23:28:13 +0530

    new table added

--- src/api/file-manager/file-manager.controller.ts
+++ src/api/file-manager/file-manager.controller.ts
@@ -70,17 +70,17 @@ export class FileManagerController {
 		@Param('rocketShipId') rocketShipId: string,
 		@Body() body: string
 	) {
-		const { label, } = 
-		try {
-			if (!rocketShipId) {
-				return errorResponse('rocketShipId not provided', 400);
-			}
-			const fileManagerResponse: any =
-				await this.fileManagerService.getMedia(rocketShipId);
-			return successResponse('file fetched successfully', fileManagerResponse);
-		} catch (error) {
-			return errorResponse('Failed to list files', 400);
-		}
+		// const { label, } = 
+		// try {
+		// 	if (!rocketShipId) {
+		// 		return errorResponse('rocketShipId not provided', 400);
+		// 	}
+		// 	const fileManagerResponse: any =
+		// 		await this.fileManagerService.getMedia(rocketShipId);
+		// 	return successResponse('file fetched successfully', fileManagerResponse);
+		// } catch (error) {
+		// 	return errorResponse('Failed to list files', 400);
+		// }
 	}
 
 	@Get('file-managers/:id')
