b4d6cdab3535ecf26ac8c691d774ac21ab8732b1
Mohit Prakash <mohit.prakash@antiersolutions.com>
2024-03-04 22:14:54 +0530

    workg

--- src/api/file-manager/file-manager.controller.ts
+++ src/api/file-manager/file-manager.controller.ts
@@ -65,7 +65,7 @@ export class FileManagerController {
 		}
 	}
 
-	@Post('/get/:rocketShipId')
+	@Post('/get/:rocketShipId') 
 	async searchMedia(@Body() body:any ,  @Param('rocketShipId') rocketShipId: string) {
 		try {
 			const { label, channel, search,sort } = body;
